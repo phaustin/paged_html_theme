a1fa0b9072a0731b1216141d5365c9b160911b1c
Harlan Colclough <harlan.colclough@gmail.com>
2020-05-15 14:20:01 -0700

    added folder within static of theme

--- build_all.ps1
+++ build_all.ps1
@@ -3,6 +3,10 @@ $files = Get-ChildItem -Directory
 for ($i = 0; $i -lt $files.Count; $i++) {
     $dir = $files[$i].FullName
     Set-Location $dir
+    $folder = ".\build\"
+    if (Test-Path $folder) { Remove-Item $folder -Force -Confirm:$false; }
+    $folder = ".\jupyter_execute\"
+    if (Test-Path $folder) { Remove-Item $folder -Force -Confirm:$false; }
     $Script = $dir + "\build_website.ps1"
     &$Script
     Set-Location ..
