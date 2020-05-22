82887fbf8be44de58313b81f2ce80499e76f8e44
Phil Austin <paustin@eos.ubc.ca>
2020-05-22 16:02:06 -0700

    add packages

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='paged-html-theme',
     version="0.1",
+    packages=["paged_html_theme"],
     entry_points = {
         'sphinx.html_themes': [
             'paged_html_theme = paged_html_theme',
