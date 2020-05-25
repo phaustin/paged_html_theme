ae4a90169437968f4ed28d9420109c029edf0884
Harlan Colclough <harlan.colclough@gmail.com>
2020-05-25 13:55:41 -0700

    fixed bash syntax

--- build_theme.sh
+++ build_theme.sh
@@ -1,4 +1,4 @@
 echo "Running Sass" 
-sass --no-source-map .\src\scss\print_style.scss .\src\css\print_style.css
+sass --no-source-map ./src/scss/print_style.scss ./src/css/print_style.css
 echo "Injecting CSS"
 python.exe inject_css.py
