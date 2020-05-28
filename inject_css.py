de328c0ba1742cfd75bbe81b05ff2bd0958dad1a
Phil Austin <paustin@eos.ubc.ca>
2020-05-28 07:40:47 -0700

    tweek docstrings

--- inject_css.py
+++ inject_css.py
@@ -5,7 +5,21 @@ import sass
 
 def compile_scss(in_scss, out_css):
     """
-    compile in_scss to out_css
+    compile an scss file to css
+    
+    Parameters
+    ----------
+
+    in_scss: str
+       path to scss file
+
+    out_css: str
+       path to ouput css file
+
+    Returns
+    -------
+
+    None
 
     """
     css_dir = Path(out_css).parent
@@ -47,10 +61,19 @@ def inject_css(style_file, template_j2, layout_html):
     with open(layout_html,'w', encoding="utf8") as output_f:
         output_f.write(output_str)
 
-if __name__ == "__main__":
+@click.command()
+def main():
+    """\b
+    script to compile print_style.scss into css and inject it
+    into the jinja2 template layout.html
+    """
     in_scss = str(context.root_dir / 'src/scss/print_style.scss')
     out_css = str(context.root_dir / 'src/css/print_style.css')
     compile_scss(in_scss, out_css)
     template_j2 = str(context.root_dir / 'src/template.html')
     layout_j2 = str(context.root_dir / 'paged_html_theme/layout.html')
     inject_css(out_css, template_j2, layout_j2)
+    print(f"injected {out_css}\n into {template_j2}\n and wrote {layout_j2}")
+        
+if __name__ == "__main__":
+    main()
